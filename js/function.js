434eba68bfae87d86aa252f0f2c8cf836bdfc596
darknawg63 <darknawg63@gmail.com>
2015-03-05 19:51:46 +0100

    Project completed

---
+++ js/function.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.awesome-form .input-group input').focusout(function(){
+        
+        var text_val = $(this).val();
+
+        if(text_val === "") {
+            $(this).removeClass('has-value');
+        } else {
+            $(this).addClass('has-value');
+        }
+    });
+});
